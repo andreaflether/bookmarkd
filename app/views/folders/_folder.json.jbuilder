8ab70e12af22e89e3e6bf71dcd676a5bafbd263d
Andréa Alencar <andreaflether@gmail.com>
2020-10-20 23:04:16 -0300

    create folder scaffold

---
+++ app/views/folders/_folder.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! folder, :id, :name, :description, :created_at, :updated_at
+json.url folder_url(folder, format: :json)
---
+++ db/migrate/20201021020313_create_folders.rb
@@ -0,0 +1,10 @@
+class CreateFolders < ActiveRecord::Migration[5.2]
+  def change
+    create_table :folders do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
