d0a11aae9dc8eae8868db1531938ba19cfe2aaca
Andréa Alencar <andreaflether@gmail.com>
2020-10-26 10:39:23 -0300

    add bootstrap-maxlength for textarea and add validations for name and description

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,5 +2,6 @@
 //= require activestorage
 //= require jquery/dist/jquery
 //= require bootstrap/dist/js/bootstrap
+//= require bootstrap-maxlength/dist/bootstrap-maxlength
 //= require general
 //= require_tree .
--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,4 +1,7 @@
 class Folder < ApplicationRecord
   belongs_to :user 
   has_and_belongs_to_many :tweets
+
+  validates_presence_of :name
+  validates_length_of :description, maximum: 200
 end
