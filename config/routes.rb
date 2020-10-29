39ad4dd691bcc11f2288daf9e42cc21fe2e76a22
Andréa Alencar <andreaflether@gmail.com>
2020-10-29 11:56:08 -0300

    routes based on user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :tweets
   resources :folders
-  root 'pages#home'
+
+  authenticated :user do root to: 'folders#index' end
+  unauthenticated :user do root to: 'pages#home' end
+  
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end
