838ea0938c62b78164a4f23195f10027249ccbb7
Andréa Alencar <andreaflether@gmail.com>
2020-10-30 17:42:28 -0300

    add custom flash messages with toastr

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def custom_flash_messages
+    flash_messages = []
+
+    # Overall flash messages
+    flash.each do |type, message|
+      type = 'success' if type == 'notice'
+      type = 'error'   if type == 'alert'
+      type = 'info'    if type == 'info'
+      text = "<script>$( document ).ready(function() {toastr.#{type}('#{message}');});</script>"
+      flash_messages << text.html_safe
+    end
+
+    flash_messages.join("\n").html_safe
+  end
 end
