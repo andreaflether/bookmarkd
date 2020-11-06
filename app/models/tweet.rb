e442e83682b81db450a8751bc55fe4e9b140fdd0
Andréa Alencar <andreaflether@gmail.com>
2020-11-06 11:45:29 -0300

    add restriction for adding tweets from a private accoutn

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -9,14 +9,15 @@ class Tweet < ApplicationRecord
     message: 'Incorrect format for Twitter URL!',
     allow_blank: true
   }
-  # ,
-  # uniqueness: { message: 'Tweet already exists in the database. ' }
+  
   validate :return_html_content
 
   def return_html_content 
     html_hash = { html_content: EmbedTweetService.new(self.link).fetch }
     content = html_hash[:html_content]
-    if content.code != 200
+    if content.code == 403
+      self.errors.add(:link, 'Only tweets from public accounts are allowed.')
+    elsif content.code != 200
       self.errors.add(:link, 'Tweet URL is not valid!')
     else 
       html = JSON.parse(html_hash[:html_content].body)['html']
