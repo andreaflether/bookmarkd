d297fe62e37c74d0f874d0a5fc7b6d878883a456
Andréa Alencar <andreaflether@gmail.com>
2020-12-26 12:40:28 -0300

    add content_for title to display dynamic and static title for pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,10 +7,33 @@ module ApplicationHelper
       type = 'success' if type == 'notice'
       type = 'error'   if type == 'alert'
       type = 'info'    if type == 'info'
-      text = "<script>$( document ).ready(function() {toastr.#{type}('#{message}');});</script>"
+      text = "
+      <script>
+        $(document).ready(function(){
+          toastr.#{type}('#{message}');
+        });
+      </script>
+      "
       flash_messages << text.html_safe
     end
-
     flash_messages.join("\n").html_safe
+  end
+
+  def title
+    if content_for?(:title)
+      # allows the title to be set in the view by using t(".title")
+      page_title = content_for(:title)
+    else
+      page_title = t("#{ controller_path.tr('/', '.') }.#{ action_name }")
+    end
+    page_title.include?('translation_missing') ? t(:site_name) : "#{page_title} / #{t(:site_name)}"
+  end
+
+  def light_mode? 
+    cookies[:sun]
+  end
+
+  def application_mode 
+    light_mode? ? 'light' : 'dark'
   end
 end
