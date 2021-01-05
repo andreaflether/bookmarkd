fc6d4b79fbe6f7ce0800bf33e0a408da0a3fc48b
Andréa Alencar <andreaflether@gmail.com>
2021-01-05 19:06:16 -0300

    improvement in registrations edit page and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,30 @@
 class User < ApplicationRecord
-  attr_accessor :terms
-
-  validates_acceptance_of :terms, message: 'You need to accept the terms and conditions.'
-
-  typed_store :preferences do |s|
-    s.string :order_folders_by, default: 'original-order', null: false
-  end
-
-  validates :order_folders_by, inclusion: { 
-    in: %w(original-order number_of_tweets name),
-    message: 'Please provide a valid filter.'
-  }
-
-  has_many :folders
-  validates :name, presence: true, length: { maximum: 50, allow_blank: true } 
-
+  # Devise
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, 
          :omniauthable, omniauth_providers: %i[twitter]
+
+  # Virtual attributes
+  attr_accessor :terms
+
+  # Relationships
+  has_many :folders
+  
+  # Preferences
+  typed_store :preferences do |s|
+    s.string :order_folders_by, default: 'original-order', null: false
+  end
+  
+  # Validations
+  validates :name, presence: true, length: { maximum: 50, allow_blank: true } 
+
+  validates_acceptance_of :terms, message: 'You need to accept the terms and conditions.'
+
+  # Validations: Preferences
+  validates :order_folders_by, inclusion: { 
+    in: %w(original-order number_of_tweets name),
+    message: 'Please provide a valid filter.'
+  }  
 
   def self.from_omniauth(auth)
     user = find_or_initialize_by(provider: auth.provider, uid: auth.uid)
@@ -37,7 +44,7 @@ class User < ApplicationRecord
     end
     pfp_url
   end 
-
+  
   def pinned_folders
     folders.where(pinned: true)
   end
