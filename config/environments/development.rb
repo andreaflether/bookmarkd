792e0de5ce35871d9be311ac42c39da839b1f13a
Andréa Alencar <andreaflether@gmail.com>
2021-01-10 18:29:44 -0300

    add mailcatcher config in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,13 @@ Rails.application.configure do
   # Devise mailer
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  # Mailcatcher
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+  config.action_mailer.raise_delivery_errors = false
+  config.action_controller.asset_host = 'http://localhost:3000'
+  config.action_mailer.asset_host = config.action_controller.asset_host
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
