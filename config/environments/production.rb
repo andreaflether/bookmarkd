80ec04aad70778bfa0b7247a5ea8c035acd4355a
Andréa Alencar <andreaflether@gmail.com>
2021-01-10 19:44:37 -0300

    add bookmark logo in pre compiled assets and add config for production asset_host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,8 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = 'bookmarkdd.herokuapp.com'
+  config.action_mailer.asset_host = config.action_controller.asset_host
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
