7f6493b267c2530796fd713114e4464e6a0dd326
Andréa Alencar <andreaflether@gmail.com>
2021-02-07 18:36:44 -0300

    change password min length (6 --> 8)

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,8 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   # Omniauth config
-  config.omniauth :twitter, Rails.application.credentials.twitter_app_key, Rails.application.credentials.twitter_app_secret
+  config.omniauth :twitter, Rails.application.credentials.twitter_app_key,
+                  Rails.application.credentials.twitter_app_secret
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
@@ -180,7 +181,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length.
-  config.password_length = 6..128
+  config.password_length = 8..128
 
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     email { Faker::Internet.email }
     name { Faker::Name.name }
     username { Faker::Internet.username(specifier: 4..15, separators: %w[_]) }
-    password { '123456' }
-    password_confirmation { '123456' }
+    password { '12345678' }
+    password_confirmation { '12345678' }
   end
 end
