c61a6d80d1890350f2547ff8db0d6b45fafc4b8b
Andréa Alencar <andreaflether@gmail.com>
2021-03-07 11:29:17 -0300

    organizing views structure

--- spec/requests/folders/folders_js_spec.rb
+++ spec/requests/folders/folders_js_spec.rb
@@ -26,8 +26,6 @@ RSpec.describe 'Folder - JS', type: :request do
       expect(response.body).to include(I18n.t('controllers.folders.tweet.added',
                                               folder: folder_params_with_tweet[:name]))
     }
-
-    it { expect(response).to render_template(:append_tweet) }
   end
 
   describe 'PUT /folders/pin_folder/:id, format: :js' do
@@ -35,24 +33,24 @@ RSpec.describe 'Folder - JS', type: :request do
     let(:pinned_folder) { create(:folder, pinned: true, user: user) }
 
     context 'when folder is NOT pinned' do
-      before { put toggle_pin_folders_path(unpinned_folder), params: { session: login_as(user), format: :js } }
+      before { put toggle_pin_folder_path(unpinned_folder), params: { session: login_as(user), format: :js } }
 
       it { expect { unpinned_folder.reload.pinned }.to change(unpinned_folder, :pinned).from(false).to(true) }
       it { expect(response.body).to include(I18n.t('controllers.folders.pin', folder: unpinned_folder.name)) }
     end
 
     context 'when folder is pinned' do
-      before { put toggle_pin_folders_path(pinned_folder), params: { session: login_as(user), format: :js } }
+      before { put toggle_pin_folder_path(pinned_folder), params: { session: login_as(user), format: :js } }
 
       it { expect { pinned_folder.reload.pinned }.to change(pinned_folder, :pinned).from(true).to(false) }
       it { expect(response.body).to include(I18n.t('controllers.folders.unpin', folder: pinned_folder.name)) }
     end
 
-    context 'when user already has 10 pinned folders and tries do pin another one' do
-      let!(:folders) { create_list(:folder, 10, pinned: true, user: user) }
+    context 'when user already has 15 pinned folders and tries do pin another one' do
+      let!(:folders) { create_list(:folder, 15, pinned: true, user: user) }
       let(:invalid_folder) { create(:folder, user: user) }
 
-      before { put toggle_pin_folders_path(invalid_folder), params: { session: login_as(user), format: :js } }
+      before { put toggle_pin_folder_path(invalid_folder), params: { session: login_as(user), format: :js } }
 
       it {
         expect(response.body).to include(
@@ -60,8 +58,6 @@ RSpec.describe 'Folder - JS', type: :request do
                  max_pins: folder.class.const_get('MAX_PINNED_FOLDERS'))
         )
       }
-
-      it { expect(response).to render_template(:error) }
     end
   end
 
@@ -71,7 +67,5 @@ RSpec.describe 'Folder - JS', type: :request do
     before { delete folder_path(deleted_folder), params: { session: login_as(user), format: :js } }
 
     it { expect(response.body).to include(I18n.t('controllers.folders.deleted')) }
-
-    it { expect(response).to render_template(:destroy) }
   end
 end
